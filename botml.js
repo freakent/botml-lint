c08a90bb631981d4140813577905c8ff08594b76
Martin Jarvis <martin.jarvis@oracle.com>
2020-01-31 16:31:42 +0000

    Added some extra comments to loader

--- botml.js
+++ botml.js
@@ -6,13 +6,17 @@ const assert = require("assert")
 // read a zip file and extract yaml file
 // This would probably be more performant if this was a singleton that cached the result
 async function load() {
+  // Load in the zip file specified by the enviornment variable skillbot
   let file = fs.readFileSync(process.env.skillbot || "./data/sample.zip")
   let zip = await JSZip.loadAsync(file)
 
   // Big assumption here
   assert.equal(zip.file(/dialogs\/.*\.yaml/).length, 1, "this module expects skills to have only 1 dialog yaml (botml) files")
 
+  // Find the first .yaml file in the dialogs directory inside the zip file
   let data = await zip.file(/dialogs\/.*\.yaml/)[0].async("string")
+
+  // Parse the YAML into something more easy to process in JavaScript
   let botml = YAML.parse(data)
   return botml
 }
